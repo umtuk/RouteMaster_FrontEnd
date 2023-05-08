9fbdaf193073e43c0504ed4676b62eee08ad1d9a
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-05-09 04:32:26 +0900

    feat: create email login component

---
+++ src/components/Login/EmailLogin/EmailLogin.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function EmailLogin(): JSX.Element {
+  return (
+    <>
+      <div>Here is EmailLogin Pages</div>
+      <input type="text" />
+      <input type="text" />
+      <button type="submit">Login</button>
+    </>
+  );
+}
+
+export default EmailLogin;
