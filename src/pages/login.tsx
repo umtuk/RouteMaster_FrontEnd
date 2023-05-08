ed918a2155a64a47d28f0363a554ce8d9471edbf
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-05-09 04:36:12 +0900

    feat: add GoogleLogin and EmailLogin components

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import EmailLogin from '../components/Login/EmailLogin/EmailLogin';
+import GoogleLogin from '../components/Login/GoogleLogin/GoogleLogin';
 
 function Login(): JSX.Element {
-  return <> </>;
+  return (
+    <>
+      <div>Here is Login Pages</div>
+      <br />
+      <EmailLogin />
+      <GoogleLogin />
+    </>
+  );
 }
 
 export default Login;
