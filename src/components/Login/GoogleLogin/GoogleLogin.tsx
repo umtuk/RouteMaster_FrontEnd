3011bbd81ed280cb24d10376a4019aafa03fbbd6
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-05-09 04:35:29 +0900

    feat: create GoogleLogin component

---
+++ src/components/Login/GoogleLogin/GoogleLogin.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GoogleLogin(): JSX.Element {
+  return (
+    <button type="submit">
+        Google Login
+    </button>
+  );
+}
+
+export default GoogleLogin;
+
