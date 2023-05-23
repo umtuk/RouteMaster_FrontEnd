ef0b450d40e49c355b6a8bbe04dd11261e9f82dd
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-05-23 13:38:10 +0900

    Modify main page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import {Link, Route} from 'react-router-dom';
+import Header from '../components/Header/Header';
 
 function Main(): JSX.Element {
   return (
-    <>
-      <div>hi</div>
-      <button id="login-btn" type="button">
-        <Link to="/login">Login</Link>
-      </button>
-    </>
+    <Header />
   );
 }
 
