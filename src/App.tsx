19e989a1527d1b77f41fc240e5d425093e4b9f08
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-05-23 13:28:53 +0900

    chore: add a route to plan-list page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './pages/main';
 import Login from './pages/login';
+import PlanList from './pages/plan-list';
 
 function App(): JSX.Element {
   return (
@@ -10,6 +11,7 @@ function App(): JSX.Element {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/plan-list" element={<PlanList />} />
         </Routes>
       </BrowserRouter>
     </div>
