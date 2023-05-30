8f4a309ddb2ddad1c1e4b5723833cb62ba41e4d8
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-05-31 00:12:08 +0900

    chore: add path to plans page

--- src/pages/plan-list.tsx
+++ src/pages/plan-list.tsx
@@ -1,5 +1,8 @@
+import { Link } from 'react-router-dom';
+import Plans from './plans';
+
 function PlanList(): JSX.Element {
-  return <> </>;
+  return <Link to="/plan-list/plans">LegoLego</Link>;
 }
 
 export default PlanList;
