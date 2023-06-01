a34b5df8acf3cc6c9d1dfef00a73686e3bf29d47
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-06-01 20:15:03 +0900

    feature: intialize and style ItemList

---
+++ src/components/ItemList/ItemList.tsx
@@ -0,0 +1,17 @@
+import LogoImg from '../../assets/images/logo_green.png';
+import styles from './ItemList.module.css';
+
+function ItemList(): JSX.Element {
+  const lists = Array.from({ length: 10 }, (_, i) => 
+    <li key={i} className={styles.body}>
+      <img src={LogoImg} alt="" />
+      <div className={styles.hotel_info}>
+        <h1>{i}. HOTEL</h1>
+        <h3>Price</h3>
+      </div>
+    </li>
+  );
+  return <ul>{lists}</ul>;
+}
+
+export default ItemList;
