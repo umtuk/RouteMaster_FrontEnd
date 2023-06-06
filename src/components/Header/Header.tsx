854c42ceac90e251740113936c136d3c58c9d068
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-06-07 06:51:20 +0900

    Modify and refactor css styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,50 +5,52 @@ import LogoImage from '../../assets/images/logo_green.png';
 function Header(): JSX.Element {
   return (
     <header className={styles.body}>
-      <div className={styles.header_top}>
-        <div className={styles.header__logo}>
+      <div className={styles.headerTop}>
+        <div className={styles.headerLogo}>
           <Link to="/">
             <img src={LogoImage} alt="logo" />
           </Link>
-          <h1>Route Master</h1>
+          <div className={styles.headerName}>
+            <h1>Route Master</h1>
+          </div>
         </div>
-        <div className={styles.header_btn_container}>
-          <button type="button" className={styles.header_btn}>
+        <div className={styles.headerBtnContainer}>
+          <button type="button" className={styles.headerBtn}>
             <Link to="/plan-list">Trips</Link>
           </button>
-          <button type="button" className={styles.header_btn}>
+          <button type="button" className={styles.headerBtn}>
             <Link to="/">Language</Link>
           </button>
-          <button id="login-btn" type="button" className={styles.header_btn}>
+          <button id="login-btn" type="button" className={styles.headerBtn}>
             <Link to="/login">Login</Link>
           </button>
         </div>
       </div>
 
-      <div className={styles.header_bottom}>
-        <ul>
+      <div className={styles.headerBottom}>
+        <ul className={styles.ul}>
           <li className={styles.li}>
-            <button type="button" className={styles.nav_btn}>
+            <button type="button" className={styles.navBtn}>
               <Link to="/hotels">Hotels</Link>
             </button>
           </li>
           <li className={styles.li}>
-            <button type="button" className={styles.nav_btn}>
+            <button type="button" className={styles.navBtn}>
               <Link to="/">Flights</Link>
             </button>
           </li>
           <li className={styles.li}>
-            <button type="button" className={styles.nav_btn}>
+            <button type="button" className={styles.navBtn}>
               <Link to="/">Activities</Link>
             </button>
           </li>
           <li className={styles.li}>
-            <button type="button" className={styles.nav_btn}>
+            <button type="button" className={styles.navBtn}>
               <Link to="/">Rentals</Link>
             </button>
           </li>
           <li className={styles.li}>
-            <button type="button" className={styles.nav_btn}>
+            <button type="button" className={styles.navBtn}>
               <Link to="/">Restaurants</Link>
             </button>
           </li>
