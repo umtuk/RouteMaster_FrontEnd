6242a79bb697176c541ae1c8e61942a00a40eb8c
Hyeongjin Lee <lhjin0j@gmail.com>
2023-06-11 18:07:24 +0900

    feature: prototype of calculation page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Login from './pages/login';
 import PlanList from './pages/plan-list';
 import Plans from './pages/plans';
 import Hotels from './pages/hotels';
+import Calculation from './pages/calculation';
 
 function App(): JSX.Element {
   return (
@@ -16,6 +17,7 @@ function App(): JSX.Element {
           <Route path="/plan-list" element={<PlanList />} />
           <Route path="/plan-list/plans" element={<Plans />} />
           <Route path="/hotels" element={<Hotels />} />
+          <Route path="/calculate/:id" element={<Calculation />} />
         </Routes>
       </BrowserRouter>
     </div>
