5e9b87c1b40e837a19aa7a04ac63ad99da26d7c4
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-06-13 13:36:38 +0900

    Add kakao to global

--- src/components/MapColumn/MapColumn.tsx
+++ src/components/MapColumn/MapColumn.tsx
@@ -1,14 +1,10 @@
 import KakaoMaps from '../KakaoMaps/KakaoMaps';
 import styles from './MapColumn.module.css';
-import SearchBar from '../SearchBar/SearchBar';
 
 function MapColumn(): JSX.Element {
   return (
     <div className={styles.body}>
       <KakaoMaps />
-      <div>
-        <SearchBar />
-      </div>
     </div>
   );
 }
