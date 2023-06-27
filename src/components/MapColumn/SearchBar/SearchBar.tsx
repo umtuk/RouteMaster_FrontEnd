7c53c500fa2898a2cfb9ec2f23a64c217a49c7fb
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-06-28 04:19:14 +0900

    feat: feature MapColumnSearchBar

---
+++ src/components/MapColumn/SearchBar/SearchBar.tsx
@@ -0,0 +1,48 @@
+import { useEffect, forwardRef } from 'react';
+import styles from './SearchBar.module.css';
+
+interface Props {
+  inputRef: React.RefObject<HTMLInputElement>;
+  placeListRef: React.RefObject<HTMLUListElement>;
+  paginationRef: React.RefObject<HTMLDivElement>;
+  setKeyword: React.Dispatch<React.SetStateAction<any>>;
+}
+
+function MapColumnSearchBar({
+  inputRef,
+  placeListRef,
+  paginationRef,
+  setKeyword,
+}: Props): JSX.Element {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setKeyword(inputRef.current?.value);
+  };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div id="option">
+        <form onSubmit={onSubmit}>
+          <input
+            id="keyword"
+            className={styles.input}
+            type="text"
+            placeholder="어디를 체험해볼까요?"
+            ref={inputRef}
+          />
+        </form>
+      </div>
+      {/* <hr />
+      <ul id="placeList" ref={placeListRef}> </ul>
+      <div id="pagination" ref={paginationRef}> </div> */}
+    </div>
+  );
+}
+
+export default forwardRef(MapColumnSearchBar);
